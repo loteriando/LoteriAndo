9f35e25a7923c47bcacbdea3638d3b73b9fa1788
loteriando <livonaldo@uol.com.br>
2026-08-25 16:01:04 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,8 @@
-const CACHE='loteriando-v2';
-const FILES=['./','./index.html','./manifest.json','./loteriando-icon.png'];
+const CACHE='loteriando-v3';
+const FILES=['./','./index.html','./manifest.json','./icon-180.png','./icon-192.png','./icon-512.png','./loteriando-icon.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('activate',e=>e.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!=='loteriando-v3').map(k=>caches.delete(k))))
+  .then(()=>self.clients.claim())
+));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
